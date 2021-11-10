ff894585c91a428bd756d45c26a470a09172315b
Cliff Pereira <cpereira@it-economics.de>
2021-11-10 08:54:28 +0100

    initial commit

---
+++ src/main/java/com/pereira/workshop/craftingcode/CraftingCodeApplication.java
@@ -0,0 +1,13 @@
+package com.pereira.workshop.craftingcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftingCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftingCodeApplication.class, args);
+    }
+
+}
